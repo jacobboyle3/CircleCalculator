d3c0b825c0b2fd70853d0bdd980efe0ec3d00f7e
Jacob <jacobbloomboyle@gmail.com>
2022-04-11 15:42:14 -0400

    Room calculator

---
+++ RoomCalculator/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Please enter the length of your room: ");
+        decimal length = decimal.Parse(Console.ReadLine());
+        Console.WriteLine("Please enter the width of your room: ");
+        decimal width = decimal.Parse(Console.ReadLine());
+        decimal area = length * width;
+        Console.WriteLine("The area of the room is: {0}", area);
+        decimal perimeter = length + width + length + width;
+        Console.WriteLine("The perimeter of your room is: {0}", perimeter);
+    }
+}
